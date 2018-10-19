a072627540f8e90ae31b2712ca57b9d26282d0f8
Seunghuk Kim <skrimm00ak@gmail.com>
2018-10-19 14:02:30 +0900

    only tweets

--- twitter/get_data.py
+++ twitter/get_data.py
@@ -11,11 +11,11 @@ twitter = OAuth1Session(CK, CS, AT, ATS)
 url = "https://api.twitter.com/1.1/search/tweets.json"
 
 print("search term:")
-keyword = input('>> ')
+keyword = input('>> ') + " -rt"
 print('-------------------------------------')
 
 #search term and number of tweets to retrieve
-params = {'q': keyword, 'count': 20}
+params = {'q': keyword, 'count': 100, 'lang':'en'}
 
 req = twitter.get(url, params = params)
 
@@ -25,9 +25,12 @@ df = pd.DataFrame(columns=["Datatime", "User", "Text"])
 if req.status_code == 200:
     search_timeline = json.loads(req.text)
     for tweet in search_timeline['statuses']:
+        #not allow mention tweets
+        if tweet['entities']['user_mentions']:
+            continue
         created_at = (tweet["created_at"])
-        User = (tweet["user"]["screen_name"].encode("utf-8"))
-        Text = (tweet["text"].encode("utf-8"))
+        User = (tweet["user"]["screen_name"])
+        Text = (tweet["text"])
         tmp_df = pd.Series([created_at, User, Text], index=df.columns)
 
         #append to dataframe
